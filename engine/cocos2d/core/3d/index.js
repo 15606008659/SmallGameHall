903745c239b872ac869c3e91faa255910e9533cf
WuXinyu <wxy897780310@gmail.com>
2019-07-08 16:48:04 +0800

    测试项目

---
+++ engine/cocos2d/core/3d/index.js
@@ -0,0 +1,16 @@
+
+if (!CC_TEST && (!CC_EDITOR || !Editor.isMainProcess)) {
+    require('./polyfill-3d');
+    require('./geom-utils');
+}
+
+require('./CCModel');
+require('./CCSkeleton');
+require('./CCSkeletonAnimationClip');
+require('./CCModelMeshResource');
+require('./actions');
+
+if (!CC_EDITOR || !Editor.isMainProcess) {
+    require('./CCSkeletonAnimation');
+    require('./CCSkinnedMeshRenderer');
+}
