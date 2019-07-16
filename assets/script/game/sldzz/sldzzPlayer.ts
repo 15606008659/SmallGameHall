56a0ef969f03f4a8ed1c3fc108d82463415d4d9a
lichang <253138189@qq.com>
2019-07-16 17:27:13 +0800

    扫雷大作战

---
+++ assets/script/game/sldzz/sldzzPlayer.ts
@@ -0,0 +1,25 @@
+
+const {ccclass, property} = cc._decorator;
+
+@ccclass
+export default class sldzzPlayer extends cc.Component {
+    @property(cc.Sprite)
+    headSp: cc.Sprite = null;
+
+    @property(cc.Label)
+    nameLab: cc.Label = null;
+
+    @property(cc.Label)
+    scoreLab: cc.Label = null;
+
+    @property
+    seatNum: number = 0;
+
+    // onLoad () {}
+
+    start () {
+        this.scoreLab.string = "0";
+    }
+
+    // update (dt) {}
+}
